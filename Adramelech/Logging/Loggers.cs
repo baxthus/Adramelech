9609b3f37aacf196e428e9ff92c8adaaac846803
baxthus <baxthus@proton.me>
2023-12-20 17:01:47 -0300

    Add Sentry error logging
    Closes ROSE-58

--- Adramelech/Logging/Loggers.cs
+++ Adramelech/Logging/Loggers.cs
@@ -1,14 +1,29 @@
-﻿using Serilog;
+﻿using Sentry.Serilog;
+using Serilog;
 using Serilog.Core;
+using Serilog.Events;
 
 namespace Adramelech.Logging;
 
 public static class Loggers
 {
+    private static readonly Action<SentrySerilogOptions> SentryOptions = o =>
+    {
+        o.MinimumBreadcrumbLevel = LogEventLevel.Debug;
+        o.MinimumEventLevel = LogEventLevel.Error;
+        o.Dsn = Environment.GetEnvironmentVariable("SENTRY_DSN");
+        o.AttachStacktrace = true;
+        o.SendDefaultPii = true;
+    };
+
     public static readonly Logger Default = new LoggerConfiguration()
         .MinimumLevel.Debug()
         .Enrich.FromLogContext()
         .WriteTo.Console()
+// Don't send to Sentry in debug mode
+#if !DEBUG
+.WriteTo.Sentry(SentryOptions)
+#endif
         .CreateLogger();
 
     public static readonly Logger UserContext = new LoggerConfiguration()
